6a5785dd3c76bb5e69ed901b1c14ec9dee3e282f
Tushar <83815133+Tushar-ba@users.noreply.github.com>
2024-12-03 09:44:57 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,25 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.27",
+  solidity: {
+      version: '0.8.27',
+      settings: {
+          optimizer: {
+              enabled: true,
+              runs: 200,
+          },
+      },
+  },
+  networks: {
+      mumbai: {
+          url: "https://rpc-amoy.polygon.technology/",
+          accounts: ["your private key"],
+          chainId: 80002,
+      },
+  },
+  etherscan: {
+      apiKey: {
+            polygonAmoy:"your Polygon API key"
+      },
+    },
 };
